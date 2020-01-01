7b4c5bce8a506285163a83048f039f1e3dae18b1
Tim Smith <tim14smith@gmail.com>
2020-01-01 15:55:41 -0600

    Collisions in V version

--- rainbowball-v/rainbowball-game.v
+++ rainbowball-v/rainbowball-game.v
@@ -1,7 +1,6 @@
 module main
 
 import time
-import math
 import gx
 import gg
 import glfw
@@ -14,61 +13,61 @@ const (
 	TextSize = 12
 	TimerPeriod = 15
 	text_cfg = gx.TextCfg{
-		align:gx.ALIGN_LEFT
-		size:TextSize
-		color:gx.rgb(0, 0, 0)
+		align: gx.ALIGN_LEFT
+		size: TextSize
+		color: gx.rgb(0, 0, 0)
 	}
 	over_cfg = gx.TextCfg{
-		align:gx.ALIGN_LEFT
-		size:TextSize
-		color:gx.White
+		align: gx.ALIGN_LEFT
+		size: TextSize
+		color: gx.White
 	}
-	Colors = [
-		gx.rgb(255, 242, 0),    // yellow
-		gx.rgb(174, 0, 255),    // purple
-		gx.rgb(33, 66, 255),    // blue
-		gx.rgb(74, 198, 255),   // lightblue
-		gx.rgb(60, 255, 0),     // green
-		gx.rgb(255, 0, 0),      // red
-		gx.rgb(255, 180, 31)    // orange
+	Colors = [gx.rgb(255, 242, 0), // yellow
+	gx.rgb(174, 0, 255), // purple
+	gx.rgb(33, 66, 255), // blue
+	gx.rgb(74, 198, 255), // lightblue
+	gx.rgb(60, 255, 0), // green
+	gx.rgb(255, 0, 0), // red
+	gx.rgb(255, 180, 31) // orange
 	]
-
 	BackgroundColor = gx.White
 	UIColor = gx.Red
 )
 
 enum GameState {
-        paused running gameover
+	paused
+	running
+	gameover
 }
 
 struct Game {
-	mut:
-	// State of the current game
-	state    GameState
+mut:
+// State of the current game
+	state         GameState
 	// Position of the ball
-	pos_x        int
-	pos_y        int
+	pos_x         int
+	pos_y         int
 	current_color int
-	up_pressed bool
-	down_pressed bool
-	left_pressed bool
+	up_pressed    bool
+	down_pressed  bool
+	left_pressed  bool
 	right_pressed bool
-	frames_moved int
+	frames_moved  int
 	// gg context for drawing
-	gg          &gg.GG
+	gg            &gg.GG
 	// ft context for font drawing
-	ft          &freetype.FreeType
-	font_loaded bool
+	ft            &freetype.FreeType
+	font_loaded   bool
 }
-
 
 fn main() {
 	glfw.init_glfw()
 	mut game := &Game{
-		gg: gg.new_context(gg.Cfg {
+		gg: gg.new_context(gg.Cfg{
 			width: WinSize
 			height: WinSize
 			use_ortho: true // This is needed for 2D drawing
+			
 			create_window: true
 			window_title: 'V Rainbowball'
 			window_user_ptr: 0
@@ -92,7 +91,7 @@ fn main() {
 		scale: 2
 		window_user_ptr: 0
 	})
-	game.font_loaded = (game.ft != 0 )
+	game.font_loaded = (game.ft != 0)
 	for {
 		gg.clear(BackgroundColor)
 		game.draw_scene()
@@ -120,27 +119,40 @@ fn (g mut Game) run() {
 
 fn (g mut Game) draw_ui() {
 	if g.font_loaded {
-		g.ft.draw_text(100, 100,'WOOOO', text_cfg)
+		g.ft.draw_text(100, 100, 'Arrow keys to move. Esc to quit.', text_cfg)
 		if g.state == .gameover {
-			g.gg.draw_rect(0, WinSize / 2 - TextSize, WinSize,
-		 								5 * TextSize, UIColor)
+			g.gg.draw_rect(0, WinSize / 2 - TextSize, WinSize, 5 * TextSize, UIColor)
 			g.ft.draw_text(1, WinSize / 2 + 0 * TextSize, 'Game Over', over_cfg)
 			g.ft.draw_text(1, WinSize / 2 + 2 * TextSize, 'Space to restart', over_cfg)
-		} else if g.state == .paused {
-			g.gg.draw_rect(0, WinSize / 2 - TextSize, WinSize,
-				5 * TextSize, UIColor)
+		}
+		else if g.state == .paused {
+			g.gg.draw_rect(0, WinSize / 2 - TextSize, WinSize, 5 * TextSize, UIColor)
 			g.ft.draw_text(1, WinSize / 2 + 0 * TextSize, 'Game Paused', text_cfg)
 			g.ft.draw_text(1, WinSize / 2 + 2 * TextSize, 'SPACE to resume', text_cfg)
 		}
 	}
-	//g.gg.draw_rect(0, BlockSize, WinSize, LimitThickness, UIColor)
+	// g.gg.draw_rect(0, BlockSize, WinSize, LimitThickness, UIColor)
 }
 
 fn (g mut Game) draw_scene() {
 	g.draw_ui()
-	v_ud, v_lr := g.get_velocity()
+	mut v_ud, mut v_lr := g.get_velocity()
 	g.pos_x = g.pos_x + v_lr * 5
+	if g.pos_x <= 0 {
+		g.pos_x = 0
+		v_lr = 0
+	} else if g.pos_x >= (WinSize - 30) {
+		g.pos_x = WinSize - 30
+		v_lr = 0
+	}
 	g.pos_y = g.pos_y + v_ud * 5
+	if g.pos_y <= 0 {
+		g.pos_y = 0
+		v_ud = 0
+	} else if g.pos_y >= (WinSize - 30) {
+		g.pos_y = WinSize - 30
+		v_ud = 0
+	}
 	if v_ud != 0 || v_lr != 0 {
 		g.frames_moved++
 		if g.frames_moved > 4 {
@@ -152,14 +164,13 @@ fn (g mut Game) draw_scene() {
 }
 
 // Get velocity left/right and up/down. Neg means left/up, Pos means right/down
-fn (g Game) get_velocity() (int, int) {
-	up := if g.up_pressed {-1} else {0}
-	down := if g.down_pressed {1} else {0}
-	left := if g.left_pressed {-1} else {0}
-	right := if g.right_pressed {1} else {0}
-	return (up + down), (left + right)
+fn (g Game) get_velocity() (int,int) {
+	up := if g.up_pressed { -1 } else { 0 }
+	down := if g.down_pressed { 1 } else { 0 }
+	left := if g.left_pressed { -1 } else { 0 }
+	right := if g.right_pressed { 1 } else { 0 }
+	return (up + down),(left + right)
 }
-
 
 fn (g mut Game) next_color() {
 	g.current_color = (g.current_color + 1) % Colors.len
@@ -175,42 +186,42 @@ fn key_down(wnd voidptr, key, code, action, mods int) {
 	// global keys
 	if action == GlfwPressAction {
 		match key {
-		glfw.KEY_ESCAPE {
-			glfw.set_should_close(wnd, true)
-		}
-		glfw.key_space {
-			if game.state == .running {
-				game.state = .paused
-			} else if game.state == .paused {
-				game.state = .running
-			} else if game.state == .gameover {
-				game.init_game()
-				game.state = .running
+			glfw.KEY_ESCAPE {
+				glfw.set_should_close(wnd, true)
 			}
-		}
-		else {}
+			glfw.key_space {
+				if game.state == .running {
+					game.state = .paused
+				}
+				else if game.state == .paused {
+					game.state = .running
+				}
+				else if game.state == .gameover {
+					game.init_game()
+					game.state = .running
+				}
+			}
+			else {}
 	}
 	}
-
 	if game.state != .running {
 		return
 	}
 	// keys while game is running
 	pressed := action == GlfwPressAction
-
 	match key {
-	glfw.KeyUp {
-		game.up_pressed = pressed
-	}
-	glfw.KeyLeft {
-		game.left_pressed = pressed
-	}
-	glfw.KeyRight {
-		game.right_pressed = pressed
-	}
-	glfw.KeyDown {
-		game.down_pressed = pressed
-	}
-	else { }
+		glfw.KeyUp {
+			game.up_pressed = pressed
+		}
+		glfw.KeyLeft {
+			game.left_pressed = pressed
+		}
+		glfw.KeyRight {
+			game.right_pressed = pressed
+		}
+		glfw.KeyDown {
+			game.down_pressed = pressed
+		}
+		else {}
 	}
 }
